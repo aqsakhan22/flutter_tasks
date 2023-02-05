dd6b303d6d8a8d9580203d9d458c3a0c179695fa
aqsakhan22 <https://github.com/aqsakhan22>
2023-02-05 20:38:17 +0500

    Chrome cast Integration

--- lib/Screens/ChromeCast/plugin_example_code.dart
+++ lib/Screens/ChromeCast/plugin_example_code.dart
@@ -37,46 +37,96 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<List<CastDevice>>(
-      future: _future,
-      builder: (context, snapshot) {
-        if (snapshot.hasError) {
-          return Center(
-            child: Text(
-              'Error: ${snapshot.error.toString()}',
-            ),
-          );
-        } else if (!snapshot.hasData) {
-          return Center(
-            child: CircularProgressIndicator(),
-          );
-        }
+    return ElevatedButton(onPressed: (){
 
-        if (snapshot.data!.isEmpty) {
-          return Column(
-            children: [
-              Center(
-                child: Text(
-                  'No Chromecast founded',
-                ),
+         showDialog(
+             // useSafeArea:false,
+             context: context,
+             builder: (BuildContext context){
+               return AlertDialog(
+                 contentPadding: EdgeInsets.zero,
+                 content: CastDevices(),
+               );
+             }
+
+         );
+
+      // showModalBottomSheet(
+      //     context: context,
+      //     builder: (BuildContext context) {
+      //
+      //      return
+      //
+      //        CastDevices();
+      //       //  Wrap(
+      //       //   children: [
+      //       //     ListTile(
+      //       //       leading: Icon(Icons.share),
+      //       //       title: Text('Share'),
+      //       //     ),
+      //       //     ListTile(
+      //       //       leading: Icon(Icons.copy),
+      //       //       title: Text('Copy Link'),
+      //       //     ),
+      //       //   ],
+      //       // );
+      //     }
+      //
+      // );
+
+    }, child: Text("check cast devices"));
+
+
+  }
+
+  Widget CastDevices(){
+   return Padding(
+     padding: const EdgeInsets.symmetric(vertical: 10.0),
+     child: FutureBuilder<List<CastDevice>>(
+        future: _future,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Center(
+              child: Text(
+                'Error: ${snapshot.error.toString()}',
               ),
-            ],
-          );
-        }
-
-        return Column(
-          children: snapshot.data!.map((device) {
-            return ListTile(
-              title: Text(device.name),
-              onTap: () {
-                // _connectToYourApp(context, device);
-                _connectAndPlayMedia(context, device);
-              },
             );
-          }).toList(),
-        );
-      },
-    );
+          } else if (!snapshot.hasData) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          if (snapshot.data!.isEmpty) {
+            return Column(
+              mainAxisSize: MainAxisSize.min,
+
+              children: [
+                Center(
+                  child: Text(
+                    'No Chromecast founded',
+                  ),
+                ),
+              ],
+            );
+          }
+
+          return Column(
+            mainAxisSize: MainAxisSize.min,
+            children: snapshot.data!.map((device) {
+              return ListTile(
+                leading: Icon(Icons.tv),
+                title: Text(device.name),
+                onTap: () {
+                  // _connectToYourApp(context, device);
+                  _connectAndPlayMedia(context, device);
+                },
+              );
+            }).toList(),
+          );
+        },
+      ),
+   );
   }
 
   void _startSearch() {
